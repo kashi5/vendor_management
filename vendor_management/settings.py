4135d9e9fc59d09fca5e61b1218564536e481b76
alok <kashi560@gmail.com>
2024-05-01 11:41:57 +0530

    fixed the config

--- vendor_management/settings.py
+++ vendor_management/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c+uu8!3th4bhd=xjot3nk71kzwmao744)hl*8=pjr6m^!vv%-v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
