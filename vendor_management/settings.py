5b47dcda0d948cd2aa2a1e513cba05cf87895acf
alok <kashi560@gmail.com>
2024-05-05 01:02:26 +0530

    removed comments and unnecessary files

--- vendor_management/settings.py
+++ vendor_management/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
-    # 'oauth2_provider',
     'rest_framework',
     'vendors',
 ]
